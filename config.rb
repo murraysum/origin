122618b72effd53aa90e02c70f5237f97e23153d
Murray Summers <murray.sum@gmail.com>
2016-06-06 19:49:28 +0100

    Test.

--- config.rb
+++ config.rb
@@ -44,6 +44,7 @@ configure :build do
   # Minify Javascript on build
   # activate :minify_javascript
   set :relative_links, true
+  set :http_prefix, "/origin"
 end
 
 activate :deploy do |deploy|
